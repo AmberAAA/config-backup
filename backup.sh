a7182e9c36a54e9d73df59cce2ffc276651acdad
Amber <anborong1314@gmail.com>
2023-08-03 14:54:00 +0800

    定时任务

--- backup.sh
+++ backup.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
 dir=$(dirname $0)
-date=$(date )
+date=$(date "+%Y-%m-%d %H:%M:%S")
+echo "同步助手V1 \n 开始同步配置至github \n"
+echo "时间：$(date)"
+
 cd $dir
-
-
 rm -rf ./nvim
-
 source="$HOME/.config/nvim"
-
 cp -r $source ./nvim
 git add .
 git commit -am "定时任务"
